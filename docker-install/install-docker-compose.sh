cbbf10b6cdbb6b60d2763f82bec9e08d7ebcdc07
古永忠 (Yung-Chung Ku) <ycku@csie.org>
2017-11-05 14:11:58 +0800

    Delete install-docker-compose.sh

--- docker-install/install-docker-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Tested on Ubuntu 17.04
-
-git clone https://github.com/docker/compose.git
-sudo apt install python-pip
-cd compose
-sudo pip install -r requirements.txt
-python setup.py build
-sudo python setup.py install
